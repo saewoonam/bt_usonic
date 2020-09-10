4adda8eeb5c9eda928b4cf8b08b72b33dbb2860a
Sae Woo Nam <nams@nist.gov>
2020-09-09 22:49:43 -0600

    Start to add code to play an alarm on the speaker

---
+++ usound/alarm.c
@@ -0,0 +1,121 @@
+/*
+ * alarm.c
+ *
+ *  Created on: Sep 9, 2020
+ *      Author: nams
+ */
+#include "em_timer.h"
+#include "em_cmu.h"
+// #include "em_emu.h"
+// #include "em_chip.h"
+#include "em_gpio.h"
+
+#define ALARM_FREQ 4000
+#define C4
+
+void initTIMER1_alarm(void) {
+	uint32_t timerFreq = 0;
+	// Initialize the timer
+	TIMER_Init_TypeDef timerInit = TIMER_INIT_DEFAULT;
+	// Configure TIMER1 Compare/Capture for output compare
+	TIMER_InitCC_TypeDef timerCCInit = TIMER_INITCC_DEFAULT;
+
+	timerInit.prescale = timerPrescale256;
+	timerInit.enable = false;
+	timerCCInit.mode = timerCCModeCompare;
+	timerCCInit.cmoa = timerOutputActionToggle;
+
+	// configure, but do not start timer
+	TIMER_Init(TIMER1, &timerInit);
+#ifdef C4
+
+	GPIO->TIMERROUTE[1].ROUTEEN = GPIO_TIMER_ROUTEEN_CC1PEN;
+	GPIO->TIMERROUTE[1].CC1ROUTE =
+			(gpioPortC << _GPIO_TIMER_CC1ROUTE_PORT_SHIFT)
+					| (4 << _GPIO_TIMER_CC1ROUTE_PIN_SHIFT);
+#else
+	GPIO->TIMERROUTE[1].ROUTEEN = GPIO_TIMER_ROUTEEN_CC0PEN;
+	GPIO->TIMERROUTE[1].CC0ROUTE = (gpioPortA << _GPIO_TIMER_CC0ROUTE_PORT_SHIFT)
+	| (6 << _GPIO_TIMER_CC0ROUTE_PIN_SHIFT);
+#endif
+	TIMER_InitCC(TIMER1, 1, &timerCCInit);
+
+	// Set Top value
+	// Note each overflow event constitutes 1/2 the signal period
+	timerFreq = CMU_ClockFreqGet(cmuClock_TIMER1) / (timerInit.prescale + 1);
+	int topValue = timerFreq / (2 * ALARM_FREQ) - 1;
+	TIMER_TopSet(TIMER1, topValue);
+
+	/* Start the timer */
+	//TIMER_Enable(TIMER1, true);
+}
+
+void initTIMER2(void) {
+	// Initialize the timer
+	TIMER_Init_TypeDef timerInit = TIMER_INIT_DEFAULT;
+	// Configure TIMER2 Compare/Capture for output compare
+	TIMER_InitCC_TypeDef timerCCInit = TIMER_INITCC_DEFAULT;
+
+	// Use PWM mode, which sets output on overflow and clears on compare events
+	timerInit.prescale = timerPrescale512;
+	timerInit.enable = false;
+	timerCCInit.mode = timerCCModePWM;
+
+	// Configure but do not start the timer
+	TIMER_Init(TIMER2, &timerInit);
+
+	// Configure CC Channel 0
+	TIMER_InitCC(TIMER2, 0, &timerCCInit);
+
+
+	// Set top value to overflow at the desired PWM_FREQ frequency
+	uint16_t topValue = 1<<14;
+	TIMER_TopSet(TIMER2, topValue);
+
+	// Set compare value for initial duty cycle
+	TIMER_CompareSet(TIMER2, 0, (uint32_t) (topValue>>1));
+
+	// Start the timer
+	// TIMER_Enable(TIMER2, true);
+
+	// Enable TIMER0 compare event interrupts to update the duty cycle
+	TIMER_IntEnable(TIMER2, TIMER_IEN_CC0);
+	NVIC_EnableIRQ(TIMER2_IRQn);
+}
+
+void TIMER2_IRQHandler(void) {
+	// Acknowledge the interrupt
+	static bool play = false;
+	static int count = 11;
+	uint32_t flags = TIMER_IntGet(TIMER2);
+	TIMER_IntClear(TIMER2, flags);
+
+	// printf("IRQ Handler\r\n");
+	GPIO_PinOutToggle(gpioPortB, 0);
+	TIMER_Enable(TIMER1, play);
+	if (play) {
+		TIMER_TopSet(TIMER2, 8192);
+	} else {
+		TIMER_TopSet(TIMER2, 8192);
+	}
+	play = !play;
+	count--;
+	if (count==0) {
+		TIMER_Enable(TIMER2, false);
+		TIMER_Enable(TIMER1, false);
+		set_state_start_scan();
+		//_main_state = SCAN_ADV;
+	}
+
+}
+
+void init_alarm() {
+	initTIMER1_alarm();
+	// initTIMER2();
+}
+
+void play_alarm(void) {
+	//_main_state = DISCONNECTED;
+	set_state_disconnected();
+	TIMER_Enable(TIMER2, true);
+}
---
+++ usound/alarm.h
@@ -0,0 +1,15 @@
+/*
+ * alarm.h
+ *
+ *  Created on: Sep 9, 2020
+ *      Author: nams
+ */
+
+#ifndef USOUND_ALARM_H_
+#define USOUND_ALARM_H_
+
+void init_alarm(void);
+void play_alarm(void);
+void initTIMER2(void);
+
+#endif /* USOUND_ALARM_H_ */
