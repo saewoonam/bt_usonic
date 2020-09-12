72838ebc59a6d69b3fe27b7853e0d8497088c742
Sae Woo Nam <nams@nist.gov>
2020-09-12 09:15:00 -0600

    remove GNU folder

--- GNU ARM v7.2.1 - Default/application_properties.d
+++
@@ -1,4 +0,0 @@
-application_properties.o: ../application_properties.c \
- /Users/nams/SimplicityStudio/v4_workspace/bt-usonic/platform/bootloader/api/application_properties.h
-
-/Users/nams/SimplicityStudio/v4_workspace/bt-usonic/platform/bootloader/api/application_properties.h:
--- GNU ARM v7.2.1 - Default/platform/emlib/src/em_assert.d
+++
@@ -1,4 +0,0 @@
-platform/emlib/src/em_assert.o: ../platform/emlib/src/em_assert.c \
- /Users/nams/SimplicityStudio/v4_workspace/bt-usonic/platform/emlib/inc/em_assert.h
-
-/Users/nams/SimplicityStudio/v4_workspace/bt-usonic/platform/emlib/inc/em_assert.h:
